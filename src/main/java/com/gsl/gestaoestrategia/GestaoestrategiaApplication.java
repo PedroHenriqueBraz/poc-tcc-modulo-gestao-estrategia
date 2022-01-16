be138ce95b773aa63a801bb8ae225cb2d44cb70a
Pedro Henrique Braz <pedro.henrique@opus-software.com.br>
2022-01-16 11:50:45 -0300

    setup inicial do projeto

---
+++ src/main/java/com/gsl/gestaoestrategia/GestaoestrategiaApplication.java
@@ -0,0 +1,13 @@
+package com.gsl.gestaoestrategia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoestrategiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoestrategiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gsl/gestaoestrategia/GestaoestrategiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsl.gestaoestrategia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoestrategiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

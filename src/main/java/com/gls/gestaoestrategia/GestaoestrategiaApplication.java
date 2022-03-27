b1222305ffcd0f91c312d5a876c99621508a4e45
Pedro Henrique Braz <pedro.henrique@opus-software.com.br>
2022-03-27 14:44:43 -0300

    missing files

---
+++ src/main/java/com/gls/gestaoestrategia/GestaoestrategiaApplication.java
@@ -0,0 +1,27 @@
+package com.gls.gestaoestrategia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableFeignClients
+public class GestaoestrategiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoestrategiaApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
+
+}
---
+++ src/test/java/com/gls/gestaoestrategia/GestaoestrategiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gls.gestaoestrategia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoestrategiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

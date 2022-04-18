84b6ffb8cd1bfb66502d6a7b6d78ff671261e237
Pedro Henrique Braz <pedro.henrique@opus-software.com.br>
2022-04-18 17:19:44 -0300

    health ping

---
+++ src/main/java/com/gls/gestaoestrategia/worker/HealthWorker.java
@@ -0,0 +1,26 @@
+package com.gls.gestaoestrategia.worker;
+
+import com.gls.gestaoestrategia.httpclient.mic.MICClient;
+import com.gls.gestaoestrategia.httpclient.msc.MSCClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+public class HealthWorker {
+    @Autowired
+    private MICClient micClient;
+    @Autowired
+    private MSCClient mscClient;
+
+    @Scheduled(fixedRate = 300000)
+    public void healthCheck(){
+        System.out.println("mic ping");
+        micClient.health();
+
+        System.out.println("msc ping");
+        mscClient.health();
+    }
+}
